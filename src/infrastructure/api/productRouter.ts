fcde51647c32a32bd49188f478d6ce10eead93c6
Joseph CHATELIN <josephchatelin@gmail.com>
2021-09-30 17:48:21 +0200

    update routes

--- src/infrastructure/api/productRouter.ts
+++ src/infrastructure/api/productRouter.ts
@@ -10,7 +10,7 @@ const productRouter = Router();
 productRouter.get("/", productController.getAllProduct)
 productRouter.get("/category/:name", productController.getProductByCategory)
 productRouter.get("/category/:category/brand/:brand", productController.getProductByBrand)
-productRouter.get("/info/sold", AuthorizationHandler.userAuth, AuthorizationHandler.greenRepackAuthorization, productController.getSellsNumber)
+productRouter.get("/:category/:brand/:year/sold", AuthorizationHandler.userAuth, AuthorizationHandler.greenRepackAuthorization, productController.getSellsNumber)
 productRouter.delete("/:id", AuthorizationHandler.userAuth, AuthorizationHandler.greenRepackAdminAuthorization, productController.deleteProduct)
 productRouter.get("/:id", productController.getProduct)
 
