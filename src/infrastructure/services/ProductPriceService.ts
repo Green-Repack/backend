d6a471a1f47a9a5680212c413f45ba7d8d9bdf86
DjadjiT <djadji.traore75@gmail.com>
2021-10-01 03:33:36 +0200

    add trailing zero to pricee

--- src/infrastructure/services/ProductPriceService.ts
+++ src/infrastructure/services/ProductPriceService.ts
@@ -9,10 +9,10 @@ export class ProductPriceService {
         let age = new Date().getFullYear()-year;
 
         if(age<=1) return productPrice.price/1.2;
-        else if(age<=3 && age>1) return productPrice.price/(1.2);
-        else if(age<=5 && age>3) return productPrice.price/(1.5);
-        else if(age<=10 && age>5) return productPrice.price/(1.75);
-        else if(age<=10 && age>5) return productPrice.price/(2.5);
-        else if(age>10) return productPrice.price/(5);
+        else if(age<=3 && age>1) return productPrice.price/(1.2).toFixed(2);
+        else if(age<=5 && age>3) return productPrice.price/(1.5).toFixed(2);
+        else if(age<=10 && age>5) return productPrice.price/(1.75).toFixed(2);
+        else if(age<=10 && age>5) return productPrice.price/(2.5).toFixed(2);
+        else if(age>10) return productPrice.price/(5).toFixed(2);
     }
 }
